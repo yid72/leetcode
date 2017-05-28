68ab5705165b574fc93edb19759f3ee482e534c9
Yidong Du <Yidong Du>
2017-05-28 12:08:15 -0700

    111 minimum depth of binary tree

---
+++ src/dyd/leetcode/q111/Q111_MinDepthOfBT.java
@@ -0,0 +1,33 @@
+package dyd.leetcode.q111;
+
+import dyd.leetcode.common.TreeNode;
+
+/**
+ * 
+Given a binary tree, find its minimum depth.
+
+The minimum depth is the number of nodes along the shortest path from the root node down to the nearest leaf node
+
+ * @author dyd
+ *
+ */
+public class Q111_MinDepthOfBT {
+    public int minDepth(TreeNode root) {
+        if (root == null) {
+        	return 0;
+        }
+        
+        if (root.left == null && root.right == null) {
+        	return 1;
+        }
+        else if (root.left == null) {
+        	return minDepth(root.right) + 1;
+        }
+        else if (root.right == null) {
+        	return minDepth(root.left) + 1;
+        }
+        else {
+            return Math.min(minDepth(root.left), minDepth(root.right)) + 1;
+        }
+    }
+}
