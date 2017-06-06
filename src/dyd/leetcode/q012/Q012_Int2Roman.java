dbbeaa55763034962130185c461077d04e1d97c7
dyd72 <duyidong@gmail.com>
2017-06-05 20:35:20 -0700

    12. integer to Roman

---
+++ src/dyd/leetcode/q012/Q012_Int2Roman.java
@@ -0,0 +1,51 @@
+package dyd.leetcode.q012;
+
+public class Q012_Int2Roman {
+	private static String[] romanSymbols = {"M", "CM", "DCCC", "DCC", "DC", "D", 
+				"CD", "CCC", "CC", "C", 
+				"XC", "LXXX", "LXX", "LX", "L",
+				"XL", "XXX", "XX", "X", 
+				"IX", "VIII", "VII", "VI", "V",
+				"IV", "III", "II", "I"};
+
+	private static int[] decimals = {1000, 900, 800, 700, 600, 500, 
+			400, 300, 200, 100, 
+			90, 80, 70, 60, 50,
+			40, 30, 20, 10,
+			9, 8, 7, 6, 5,
+			4, 3, 2, 1};
+	
+    public String intToRoman(int num) {
+    	if (num <= 0) {
+    		return "";
+    	}
+    	
+    	StringBuilder sb = new StringBuilder();
+    	
+        int i = 0;
+        while (num > 0) {
+        	if (num >= decimals[i]) {
+        		sb.append(romanSymbols[i]);
+        		num -= decimals[i];
+        	}
+        	else {
+        		i ++;
+        	}
+        }
+        
+        return sb.toString();
+    }
+    
+    public static void main(String[] args) {
+    	test(1);
+    	test(8);
+    	test(16);
+    	test(3000);
+    	test(2999);
+    }
+    
+    private static void test(int num) {
+    	Q012_Int2Roman q = new Q012_Int2Roman();
+    	System.out.printf("%d = %s\n", num, q.intToRoman(num));
+    }
+}
