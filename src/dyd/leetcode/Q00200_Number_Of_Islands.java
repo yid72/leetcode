b1829068a23e38ababfe5301a50c3fb0b395afa9
yid72 <duyidong@gmail.com>
2019-02-03 10:48:22 -0800

    refactor number of islands

--- src/dyd/leetcode/Q00200_Number_Of_Islands.java
+++ src/dyd/leetcode/Q00200_Number_Of_Islands.java
@@ -1,9 +1,33 @@
-package dyd.leetcode.q200;
+package dyd.leetcode;
 
 import java.util.LinkedList;
 import java.util.Queue;
 
-public class Q200_Number_Of_Islands {
+/**
+ * Given a 2d grid map of '1's (land) and '0's (water), count the number of islands. An island is surrounded by water
+ * and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid
+ * are all surrounded by water.
+ *
+ * Example 1:
+ *
+ * Input:
+ * 11110
+ * 11010
+ * 11000
+ * 00000
+ *
+ * Output: 1
+ * Example 2:
+ *
+ * Input:
+ * 11000
+ * 11000
+ * 00100
+ * 00011
+ *
+ * Output: 3
+ */
+public class Q00200_Number_Of_Islands {
 	private static class Position {
 		int i;
 		int j;
@@ -72,8 +96,7 @@ public class Q200_Number_Of_Islands {
     	}
     }
     
-    private void dfs(int i, int j, int island)
-    {
+    private void dfs(int i, int j, int island) {
     	this.flags[i][j] = island;
     	
     	if (isUnvisitedLand(i - 1, j)) {
@@ -93,21 +116,30 @@ public class Q200_Number_Of_Islands {
     	}    	
     }
     
-    private boolean isUnvisitedLand(int i, int j)
-    {
+    private boolean isUnvisitedLand(int i, int j) {
     	return (i >= 0 && i < this.grid.length &&
     			j >= 0 && j < this.grid[0].length &&
     			this.grid[i][j] == '1' && 
     			this.flags[i][j] == 0);
-    }    
-    
+    }
+
     public static void main(String[] args) {
-    	char[][] grid = {
-    			{'0', '1', '1'},
-    			{'1', '0', '1'}
+		Q00200_Number_Of_Islands t = new Q00200_Number_Of_Islands();
+
+    	char[][] input1 = {
+    			{'1', '1', '1', '1', '0'},
+    			{'1', '1', '0', '1', '0'},
+				{'1', '1', '0', '0', '0'},
+				{'0', '0', '0', '0', '0'}
     	};
-    	
-    	Q200_Number_Of_Islands t = new Q200_Number_Of_Islands();
-    	System.out.println(t.numIslands(grid));
+    	System.out.println(t.numIslands(input1));
+
+		char[][] input2 = {
+				{'1', '1', '0', '0', '0'},
+				{'1', '1', '0', '0', '0'},
+				{'0', '0', '1', '0', '0'},
+				{'0', '0', '0', '1', '1'}
+		};
+		System.out.println(t.numIslands(input2));
     }
 }
