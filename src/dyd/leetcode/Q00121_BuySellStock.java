446ccf1f8b9916f3011c6061b9d0e73af2cd53e5
yid72 <duyidong@gmail.com>
2019-04-27 08:38:23 -0700

    refactor 121. buy sell stock

--- src/dyd/leetcode/Q00121_BuySellStock.java
+++ src/dyd/leetcode/Q00121_BuySellStock.java
@@ -1,31 +1,22 @@
-package dyd.leetcode.q121;
+package dyd.leetcode;
 
 import dyd.leetcode.common.PrintUtil;
 
-public class Q121_BuySellStock {
+public class Q00121_BuySellStock {
     public int maxProfit(int[] prices) {
         if (prices == null || prices.length < 2) {
         	return 0;
         }
         
         int buy = prices[0];
-        int sell = 0;
+        int sell = buy;
         int maxProfit = 0;
         for (int i = 1; i < prices.length; i++) {
-        	if (sell == 0) {
-        		if (prices[i] > buy) {
-        			sell = prices[i];
-        		}
-        		else {
-        			buy = prices[i];
-        		}
-        	}
-        	else if (prices[i] > sell) {
+        	if (prices[i] > sell) {
         		sell = prices[i];
-        	}
-        	else if (prices[i] < buy) {
+        	} else if (prices[i] < buy) {
         		buy = prices[i];
-        		sell = -1;
+        		sell = buy;
         	}
         	maxProfit = Math.max(maxProfit, sell - buy);
         }
@@ -34,14 +25,16 @@ public class Q121_BuySellStock {
     }
     
     public static void main(String[] args) {
-    	Q121_BuySellStock q = new Q121_BuySellStock();
+    	Q00121_BuySellStock q = new Q00121_BuySellStock();
     	
     	test(new int[] {2, 4, 1});
     	test(new int[] {7, 1, 5, 3, 6, 4});
+		test(new int[] {3, 6, 2, 7, 2, 5});
+		test(new int[] {7, 6, 4, 3, 1});
     }
     
     private static void test(int[] prices) {
-    	Q121_BuySellStock q = new Q121_BuySellStock();
+    	Q00121_BuySellStock q = new Q00121_BuySellStock();
     	PrintUtil.printArray(prices);
     	System.out.println(q.maxProfit(prices));   	
     }
