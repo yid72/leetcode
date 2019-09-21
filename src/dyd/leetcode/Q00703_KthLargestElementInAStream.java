08a4ce2848c218a29674c8223124f389f599df0e
yid72 <duyidong@gmail.com>
2019-09-21 16:53:58 -0700

    704. kth largest element in a stream

--- src/dyd/leetcode/Q00703_KthLargestElementInAStream.java
+++ src/dyd/leetcode/Q00703_KthLargestElementInAStream.java
@@ -4,6 +4,22 @@ import dyd.leetcode.common.PrintUtil;
 
 import java.util.Arrays;
 
+/**
+ * Design a class to find the kth largest element in a stream. Note that it is the kth largest element in the sorted order, not the kth distinct element.
+ *
+ * Your KthLargest class will have a constructor which accepts an integer k and an integer array nums, which contains initial elements from the stream. For each call to the method KthLargest.add, return the element representing the kth largest element in the stream.
+ *
+ * Example:
+ *
+ * int k = 3;
+ * int[] arr = [4,5,8,2];
+ * KthLargest kthLargest = new KthLargest(3, arr);
+ * kthLargest.add(3);   // returns 4
+ * kthLargest.add(5);   // returns 5
+ * kthLargest.add(10);  // returns 5
+ * kthLargest.add(9);   // returns 8
+ * kthLargest.add(4);   // returns 8
+ */
 public class Q00703_KthLargestElementInAStream {
     private int[] sortedNums;
     private int k;
