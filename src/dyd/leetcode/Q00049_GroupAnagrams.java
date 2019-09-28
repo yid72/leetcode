391f035467966a1516891f6e9beb6decfece7d14
yid72 <duyidong@gmail.com>
2019-09-28 02:35:23 -0700

    49. group anagrams

---
+++ src/dyd/leetcode/Q00049_GroupAnagrams.java
@@ -0,0 +1,21 @@
+package dyd.leetcode;
+
+/**
+ * Given an array of strings, group anagrams together.
+ *
+ * Example:
+ *
+ * Input: ["eat", "tea", "tan", "ate", "nat", "bat"],
+ * Output:
+ * [
+ *   ["ate","eat","tea"],
+ *   ["nat","tan"],
+ *   ["bat"]
+ * ]
+ * Note:
+ *
+ * All inputs will be in lowercase.
+ * The order of your output does not matter.
+ */
+public class Q00049_GroupAnagrams {
+}
