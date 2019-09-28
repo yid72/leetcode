91e43d8bba9b1b9d5af796174be6e13b0d21f6e8
yid72 <duyidong@gmail.com>
2019-09-28 10:24:38 -0700

    73. set matrix zeros

--- src/dyd/leetcode/Q00073_SetMatrixZeros.java
+++ src/dyd/leetcode/Q00073_SetMatrixZeros.java
@@ -1,17 +1,47 @@
-package dyd.leetcode.q073;
+package dyd.leetcode;
 
 import java.util.HashSet;
 
 import dyd.leetcode.common.PrintUtil;
 
 /**
- * 
-Given a m x n matrix, if an element is 0, set its entire row and column to 0. Do it in place.
-
- * @author dyd
+ * Given a m x n matrix, if an element is 0, set its entire row and column to 0. Do it in place.
+ * Example 1:
+ *
+ * Input:
+ * [
+ *   [1,1,1],
+ *   [1,0,1],
+ *   [1,1,1]
+ * ]
+ * Output:
+ * [
+ *   [1,0,1],
+ *   [0,0,0],
+ *   [1,0,1]
+ * ]
+ * Example 2:
+ *
+ * Input:
+ * [
+ *   [0,1,2,0],
+ *   [3,4,5,2],
+ *   [1,3,1,5]
+ * ]
+ * Output:
+ * [
+ *   [0,0,0,0],
+ *   [0,4,5,0],
+ *   [0,3,1,0]
+ * ]
+ * Follow up:
+ *
+ * A straight forward solution using O(mn) space is probably a bad idea.
+ * A simple improvement uses O(m + n) space, but still not the best solution.
+ * Could you devise a constant space solution?
  *
  */
-public class Q073_SetMatrixZeros {
+public class Q00073_SetMatrixZeros {
     public void setZeroes(int[][] matrix) {
     	if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
     		return;
@@ -50,7 +80,7 @@ public class Q073_SetMatrixZeros {
     }
     
     public static void main(String[] args) {
-    	Q073_SetMatrixZeros q = new Q073_SetMatrixZeros();
+    	Q00073_SetMatrixZeros q = new Q00073_SetMatrixZeros();
     	
     	int[][] matrix = new int[][] {
     		{1, 2, 3, 4},
