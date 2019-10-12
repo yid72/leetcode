a9cb60f59f3471188b3bb2f3cc49281face1d5db
yid72 <duyidong@gmail.com>
2019-10-12 09:40:07 -0700

    663. equal tree partition

--- src/dyd/leetcode/Q00663_EqualTreePartition_1.java
+++ src/dyd/leetcode/Q00663_EqualTreePartition_1.java
@@ -1,61 +1,12 @@
 package dyd.leetcode;
 
-/**
- * Given a binary tree with n nodes, your task is to check if it's possible to partition the tree to two trees which have the equal sum of values after removing exactly one edge on the original tree.
- *
- * Example 1:
- * Input:
- *     5
- *    / \
- *   10 10
- *     /  \
- *    2   3
- *
- * Output: True
- * Explanation:
- *     5
- *    /
- *   10
- *
- * Sum: 15
- *
- *    10
- *   /  \
- *  2    3
- *
- * Sum: 15
- * Example 2:
- * Input:
- *     1
- *    / \
- *   2  10
- *     /  \
- *    2   20
- *
- * Output: False
- * Explanation: You can't split the tree into two trees with equal sum after removing exactly one edge on the tree.
- * Note:
- * The range of tree node value is in the range of [-100000, 100000].
- * 1 <= n <= 10000
- */
-
 import dyd.leetcode.common.TreeNode;
 import dyd.leetcode.common.TreeUtil;
 
 import java.util.HashMap;
 import java.util.Map;
 
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
- * }
- */
-
-public class Q00663_EqualTreePartition {
+public class Q00663_EqualTreePartition_1 {
     Map<TreeNode, Integer> subTreeValues = new HashMap<>();
 
     public boolean checkEqualTree(TreeNode root) {
@@ -66,7 +17,12 @@ public class Q00663_EqualTreePartition {
         subTreeValues.clear();
         computeSubTreeValues(root);
 
-        return checkEqualTree(root, subTreeValues.get(root));
+        int rootValues = subTreeValues.get(root);
+        if (rootValues % 2 != 0) {
+            return false;
+        }
+        subTreeValues.remove(root);
+        return subTreeValues.values().contains(rootValues / 2);
     }
 
     private int computeSubTreeValues(TreeNode root) {
@@ -79,31 +35,8 @@ public class Q00663_EqualTreePartition {
         return val;
     }
 
-    private boolean checkEqualTree(TreeNode node, int treeVal) {
-        if (node.left == null && node.right == null) {
-            return false;
-        }
-
-        if (node.left != null) {
-            int leftSubTreeVal =  subTreeValues.get(node.left);
-            if (leftSubTreeVal == treeVal - leftSubTreeVal) {
-                return true;
-            }
-        }
-
-        if (node.right != null) {
-            int rightSubTreeVal = subTreeValues.get(node.right);
-            if (rightSubTreeVal == treeVal - rightSubTreeVal) {
-                return true;
-            }
-        }
-
-        return (node.left != null && checkEqualTree(node.left, treeVal))
-                || (node.right != null && checkEqualTree(node.right, treeVal));
-    }
-
     public static void main(String[] args) {
-        Q00663_EqualTreePartition q = new Q00663_EqualTreePartition();
+        Q00663_EqualTreePartition_1 q = new Q00663_EqualTreePartition_1();
 
         TreeNode root1 = new TreeNode(5);
         root1.left = new TreeNode(10);
@@ -136,5 +69,11 @@ public class Q00663_EqualTreePartition {
         root5.left = new TreeNode(-1);
         TreeUtil.printTree(root5);
         System.out.println("Can be equally partitioned: " + q.checkEqualTree(root5));
+
+        TreeNode root6 = new TreeNode(1);
+        root6.right = new TreeNode(2);
+        root6.right.left = new TreeNode(2);
+        TreeUtil.printTree(root6);
+        System.out.println("Can be equally partitioned: " + q.checkEqualTree(root6));
     }
 }
