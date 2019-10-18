ee8f8e0e1f4fcd7e32220dd9d45f527d7d375614
yid72 <duyidong@gmail.com>
2019-10-17 20:21:24 -0700

    215. kth largest element in an array

--- src/dyd/leetcode/Q00215_KthLargestElementInArray.java
+++ src/dyd/leetcode/Q00215_KthLargestElementInArray.java
@@ -1,10 +1,11 @@
 package dyd.leetcode;
 
-import dyd.leetcode.common.PrintUtil;
+import java.util.Arrays;
 
 /**
  * 
-Find the kth largest element in an unsorted array. Note that it is the kth largest element in the sorted order, not the kth distinct element.
+Find the kth largest element in an unsorted array. Note that it is the kth largest element in the sorted order, not the
+ kth distinct element.
 
 For example,
 Given [3,2,1,5,6,4] and k = 2, return 5.
@@ -17,35 +18,36 @@ You may assume k is always valid, 1 <= k <= array's length.
  */
 public class Q00215_KthLargestElementInArray {
     public int findKthLargest(int[] nums, int k) {
-    	return findKthLargest(nums, 0, nums.length -1, k);
+    	return findKthLargest(nums, 0, nums.length - 1, k);
     }
     
     private int findKthLargest(int[] nums, int lo, int hi, int k) {
     	int pivot = partition(nums, lo, hi);
     	if (hi - pivot + 1 == k) {
     		return nums[pivot];
-    	}
-    	else if (hi - pivot + 1 > k) {
-    		return findKthLargest(nums, pivot, hi, k);
-    	}
-    	else {
-    		return findKthLargest(nums, lo, pivot - 1, k - (hi - lo + 1));
+    	} else if (hi - pivot + 1 > k) {
+    		return findKthLargest(nums, pivot + 1, hi, k);
+    	} else {
+    		return findKthLargest(nums, lo, pivot - 1, k - (hi - pivot + 1));
     	}
     }
     
     private int partition(int[] nums, int lo, int hi) {
-    	int pivot = nums[hi];
-    	int i = lo - 1;
-    	for (int j = lo; j < hi; j++) {
-    		if (nums[j] <= pivot) {
-    			i ++;
-    			if (i != j) {
-    				swap(nums, i, j);
-    			}
-    		}
-    	}
-    	swap(nums, i + 1, hi);
-    	return i + 1;
+    	int pivot = nums[lo];
+    	int i = lo;
+    	int j = hi + 1;
+    	while (true) {
+    		while (++i < j && nums[i] < pivot);
+    		while (--j > i && nums[j] >= pivot);
+
+    		if (i >= j) {
+    			break;
+			}
+
+    		swap(nums, i, j);
+		}
+    	swap(nums, lo, i - 1);
+    	return i - 1;
     }
     
     private void swap(int[] nums, int i, int j) {
@@ -55,13 +57,15 @@ public class Q00215_KthLargestElementInArray {
     }
     
     private static void test(int[] nums, int k) {
+    	System.out.printf("nums=%s, K=%d\n", Arrays.toString(nums), k);
     	Q00215_KthLargestElementInArray q = new Q00215_KthLargestElementInArray();
-    	PrintUtil.printArray(nums);
-    	System.out.printf("K = %d, %d", k, q.findKthLargest(nums, k));
+    	System.out.printf("result=%d\n", q.findKthLargest(nums, k));
     }
     
     public static void main(String[] args) {
-//    	test(new int[] {3, 2, 1, 5, 6, 4}, 2);
+    	test(new int[] {3, 2, 1, 5, 6, 4}, 2);
+    	test(new int[] {3, 2, 3, 1, 2, 4, 5, 5, 6}, 4);
     	test(new int[] {-1, -1}, 2);
+		test(new int[] {2, 1}, 2);
     }
 }
