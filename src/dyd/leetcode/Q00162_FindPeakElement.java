a6c555364a8073285c074c9409024e79f4896acf
yid72 <duyidong@gmail.com>
2019-11-06 20:53:27 -0800

    162. find peak element

---
+++ src/dyd/leetcode/Q00162_FindPeakElement.java
@@ -0,0 +1,64 @@
+package dyd.leetcode;
+
+import java.util.Arrays;
+
+/**
+ * A peak element is an element that is greater than its neighbors.
+ *
+ * Given an input array nums, where nums[i] ≠ nums[i+1], find a peak element and return its index.
+ *
+ * The array may contain multiple peaks, in that case return the index to any one of the peaks is fine.
+ *
+ * You may imagine that nums[-1] = nums[n] = -∞.
+ *
+ * Example 1:
+ *
+ * Input: nums = [1,2,3,1]
+ * Output: 2
+ * Explanation: 3 is a peak element and your function should return the index number 2.
+ * Example 2:
+ *
+ * Input: nums = [1,2,1,3,5,6,4]
+ * Output: 1 or 5
+ * Explanation: Your function can return either index number 1 where the peak element is 2,
+ *              or index number 5 where the peak element is 6.
+ */
+public class Q00162_FindPeakElement {
+    public int findPeakElement(int[] nums) {
+        int low = 0;
+        int high = nums.length - 1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            boolean smallRight = true;
+            if (mid < nums.length - 1) {
+                smallRight = nums[mid] > nums[mid + 1];
+            }
+
+            boolean smallLeft = true;
+            if (mid > 0) {
+                smallLeft = nums[mid] > nums[mid - 1];
+            }
+
+            if (smallRight && smallLeft) {
+                return mid;
+            } else if (!smallRight) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        test(new int[] {1,2,3,1});
+        test(new int[] {1,2,1,3,5,6,4});
+        test(new int[] {1});
+    }
+
+    private static void test(int[] nums) {
+        System.out.println(Arrays.toString(nums));
+        Q00162_FindPeakElement q = new Q00162_FindPeakElement();
+        System.out.println(q.findPeakElement(nums));
+    }
+}
