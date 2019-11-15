42747c20c2582ac48d96db045e9d00048f90a291
yid72 <duyidong@gmail.com>
2019-11-15 03:50:15 -0800

    5. Longest Palindromic Substring

--- src/dyd/leetcode/Q00005_LongestPalindromicSubstring.java
+++ src/dyd/leetcode/Q00005_LongestPalindromicSubstring.java
@@ -1,27 +1,27 @@
 package dyd.leetcode;
 
+import java.util.Arrays;
+
 /**
- * 
-Given a string s, find the longest palindromic substring in s. You may assume that the maximum length of s is 1000.
-
-Example:
-
-Input: "babad"
-
-Output: "bab"
-
-Note: "aba" is also a valid answer.
-Example:
-
-Input: "cbbd"
-
-Output: "bb"
-
- * @author dyd
+ *
+ * 5. Longest Palindromic Substring
+ *
+ * Medium
+ *
+ * Given a string s, find the longest palindromic substring in s. You may assume that the maximum length of s is 1000.
+ *
+ * Example:
+ * Input: "babad"
+ * Output: "bab"
+ * Note: "aba" is also a valid answer.
+ *
+ * Example:
+ * Input: "cbbd"
+ * Output: "bb"
  *
  */
 public class Q00005_LongestPalindromicSubstring {
-	private int maxLength = 0;
+	private int[][] dp;
 
     public String longestPalindrome(String s) {
     	if (s == null) {
@@ -32,38 +32,56 @@ public class Q00005_LongestPalindromicSubstring {
     		return s;
     	}
 
-    	int[] indices = longestPalindrome(s.toCharArray(), 0, s.length() - 1);
-    	return s.substring(indices[0], indices[1] + 1);
+    	dp = new int[s.length()][s.length()];
+    	for (int i = 0; i < s.length(); i++) {
+			Arrays.fill(dp[i], -1);
+		}
+
+    	int maxLen = 0;
+    	int start = -1;
+    	int end = -1;
+    	for (int i = 1; i < s.length(); i++) {
+    		for (int j = 0; j < i; j ++) {
+    			if (isPalindrome(s, j, i) && i - j + 1 > maxLen) {
+    				maxLen = i - j + 1;
+    				start = j;
+    				end = i;
+				}
+			}
+		}
+
+    	if (maxLen > 0) {
+    		return s.substring(start, end + 1);
+		} else {
+    		return s.substring(0, 1);
+		}
     }
 
-    private int[] longestPalindrome(char[] str, int start, int end) {
-    	System.out.println(new String(str, start, end-start+1));
-    	
-    	if (isPalinedrome(str, start, end)) {
-    		maxLength = Math.max(maxLength, end - start + 1);
-    		return new int[] {start, end};
-    	}
-    	
-    	int[] indices1 = longestPalindrome(str, start, end - 1);
-    	int[] indices2 = longestPalindrome(str, start + 1, end);
-    	
-    	return (indices1[1] - indices1[0] >= indices2[1] - indices2[0]) ? indices1 : indices2;
-    }
-    
-    private boolean isPalinedrome(char[] str, int start, int end) {
-    	while (start <= end) {
-    		if (str[start] != str[end]) {
-    			return false;
-    		}
-    		start ++;
-    		end --;
-    	}
-    	return true;
+    private boolean isPalindrome(String str, int start, int end) {
+    	if (dp[start][end] != -1) {
+    		return dp[start][end] == 1;
+		}
+
+    	if (str.charAt(start) != str.charAt(end)) {
+			dp[start][end] = 0;
+    		return false;
+		}
+
+    	if (end - start <= 1) {
+			dp[start][end] = 1;
+    		return true;
+		}
+
+    	boolean isPalindrome = isPalindrome(str, start + 1, end - 1);
+    	dp[start][end] = isPalindrome ? 1 : 0;
+    	return isPalindrome;
     }
     
     public static void main(String[] args) {
-//    	test("babad");
-//    	test("cbbd");
+    	test("a");
+		test("ab");
+    	test("babad");
+    	test("cbbd");
     	test("babaddtattarrattatddetartrateedredividerb");
     }
     
