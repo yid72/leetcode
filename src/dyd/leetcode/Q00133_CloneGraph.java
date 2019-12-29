1cc25462850bdcc56449825ab40e16e6b017353d
yid72 <duyidong@gmail.com>
2019-12-28 20:39:35 -0800

    133. clone graph

---
+++ src/dyd/leetcode/Q00133_CloneGraph.java
@@ -0,0 +1,86 @@
+package dyd.leetcode;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 133. Clone Graph -- Medium
+ *
+ * Given a reference of a node in a connected undirected graph, return a deep copy (clone) of the graph. Each node in
+ * the graph contains a val (int) and a list (List[Node]) of its neighbors.
+ *
+ * Example:
+ *
+ * Input:
+ * {"$id":"1","neighbors":[{"$id":"2","neighbors":[{"$ref":"1"},{"$id":"3","neighbors":[{"$ref":"2"},
+ * {"$id":"4","neighbors":[{"$ref":"3"},{"$ref":"1"}],"val":4}],"val":3}],"val":2},{"$ref":"4"}],"val":1}
+ *
+ * Explanation:
+ * Node 1's value is 1, and it has two neighbors: Node 2 and 4.
+ * Node 2's value is 2, and it has two neighbors: Node 1 and 3.
+ * Node 3's value is 3, and it has two neighbors: Node 2 and 4.
+ * Node 4's value is 4, and it has two neighbors: Node 1 and 3.
+ *
+ *
+ * Note:
+ *
+ * The number of nodes will be between 1 and 100.
+ * The undirected graph is a simple graph, which means no repeated edges and no self-loops in the graph.
+ * Since the graph is undirected, if node p has node q as neighbor, then node q must have node p as neighbor too.
+ * You must return the copy of the given node as a reference to the cloned graph.
+ */
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> neighbors;
+
+    public Node() {}
+
+    public Node(int _val,List<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+*/
+public class Q00133_CloneGraph {
+    class Node {
+        public int val;
+        public List<Node> neighbors;
+
+        public Node() {}
+
+        public Node(int _val,List<Node> _neighbors) {
+            val = _val;
+            neighbors = _neighbors;
+        }
+    }
+
+    public Node cloneGraph(Node node) {
+        return cloneGraph(node, new HashMap<>());
+    }
+
+    public Node cloneGraph(Node node, Map<Node, Node> map) {
+        if (map.containsKey(node)) {
+            return map.get(node);
+        }
+
+        Node newNode = new Node();
+        newNode.val = node.val;
+        map.put(node, newNode);
+
+        List<Node> newNeighbors = null;
+        if (node.neighbors != null) {
+            newNeighbors = new ArrayList<>();
+            for (Node neighbor : node.neighbors) {
+                newNeighbors.add(cloneGraph(neighbor, map));
+            }
+
+            newNode.neighbors = newNeighbors;
+        }
+
+        return newNode;
+    }
+}
