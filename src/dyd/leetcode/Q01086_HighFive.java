a4a4a68129b72d852bbe5202ece3b71787fee4fa
yid72 <duyidong@gmail.com>
2020-01-05 20:57:50 -0800

    1086. high five

---
+++ src/dyd/leetcode/Q01086_HighFive.java
@@ -0,0 +1,71 @@
+package dyd.leetcode;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
+
+/**
+ * 1086. High Five -- Easy
+ *
+ * Given a list of scores of different students, return the average score of each student's top five scores in the order
+ * of each student's id.
+ *
+ * Each entry items[i] has items[i][0] the student's id, and items[i][1] the student's score.  The average score is
+ * calculated using integer division.
+ *
+ * Example 1:
+ *
+ * Input: [[1,91],[1,92],[2,93],[2,97],[1,60],[2,77],[1,65],[1,87],[1,100],[2,100],[2,76]]
+ * Output: [[1,87],[2,88]]
+ * Explanation:
+ * The average of the student with id = 1 is 87.
+ * The average of the student with id = 2 is 88.6. But with integer division their average converts to 88.
+ *
+ *
+ * Note:
+ *
+ * 1 <= items.length <= 1000
+ * items[i].length == 2
+ * The IDs of the students is between 1 to 1000
+ * The score of the students is between 1 to 100
+ * For each student, there are at least 5 scores
+ */
+public class Q01086_HighFive {
+    public int[][] highFive(int[][] items) {
+        Map<Integer, PriorityQueue<Integer>> map = new HashMap<>();
+        for (int i = 0; i < items.length; i++) {
+            map.putIfAbsent(items[i][0], new PriorityQueue<>());
+            PriorityQueue<Integer> queue = map.get(items[i][0]);
+            queue.add(items[i][1]);
+            if (queue.size() > 5) {
+                queue.poll();
+            }
+        }
+
+        int[][] results = new int[map.size()][2];
+        int i = 0;
+        for (Map.Entry<Integer, PriorityQueue<Integer>> entry : map.entrySet()) {
+            int id = entry.getKey();
+            int avg = (int) entry.getValue().stream()
+                    .mapToInt(Integer::intValue)
+                    .average()
+                    .orElse(0);
+            results[i][0] = id;
+            results[i ++][1] = avg;
+        }
+        return results;
+    }
+
+    public static void main(String[] args) {
+        test(new int[][] {
+                {1,91},{1,92},{2,93},{2,97},{1,60},{2,77},{1,65},{1,87},{1,100},{2,100},{2,76}
+        });
+    }
+
+    private static void test(int[][] items) {
+        Q01086_HighFive q = new Q01086_HighFive();
+        System.out.printf("items=%s:\n", Arrays.deepToString(items));
+        System.out.printf("%s\n", Arrays.deepToString(q.highFive(items)));
+    }
+}
