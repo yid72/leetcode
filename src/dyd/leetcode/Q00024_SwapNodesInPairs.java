ae5b0ba382f7807dfc589eb290917a4923eb2eff
yid72 <duyidong@gmail.com>
2020-01-21 02:45:25 -0800

    refactor 24. swap nodes in pairs

--- src/dyd/leetcode/Q00024_SwapNodesInPairs.java
+++ src/dyd/leetcode/Q00024_SwapNodesInPairs.java
@@ -1,22 +1,22 @@
-package dyd.leetcode.q024;
+package dyd.leetcode;
 
 import dyd.leetcode.common.ListNode;
 import dyd.leetcode.common.ListUtil;
 import dyd.leetcode.common.PrintUtil;
 
 /**
- * 
-Given a linked list, swap every two adjacent nodes and return its head.
-
-For example,
-Given 1->2->3->4, you should return the list as 2->1->4->3.
-
-Your algorithm should use only constant space. You may not modify the values in the list, only nodes itself can be changed.
-
- * @author dyd
+ * 24. Swap Nodes in Pairs -- Medium
+ *
+ * Given a linked list, swap every two adjacent nodes and return its head.
+ *
+ * You may not modify the values in the list's nodes, only nodes itself may be changed.
+ *
+ * Example:
+ *
+ * Given 1->2->3->4, you should return the list as 2->1->4->3.
  *
  */
-public class Q24_SwapNodesInPairs {
+public class Q00024_SwapNodesInPairs {
     public ListNode swapPairs(ListNode head) {
     	if (head == null || head.next == null) {
     		return head;
@@ -48,7 +48,7 @@ public class Q24_SwapNodesInPairs {
     }
     
     public static void main(String[] args) {
-    	Q24_SwapNodesInPairs q = new Q24_SwapNodesInPairs();
+    	Q00024_SwapNodesInPairs q = new Q00024_SwapNodesInPairs();
     	
     	ListNode head = ListUtil.createList(new int[] {1, 2, 3, 4});
     	PrintUtil.printList(head);
