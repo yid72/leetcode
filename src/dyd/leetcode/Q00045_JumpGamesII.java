fbb1fcacbd5fa3004e09fdac8981d632d646a565
yid72 <duyidong@gmail.com>
2020-02-03 00:32:02 -0800

    45. jump game 2

--- src/dyd/leetcode/Q00045_JumpGamesII.java
+++ src/dyd/leetcode/Q00045_JumpGamesII.java
@@ -28,15 +28,12 @@ public class Q00045_JumpGamesII {
         }
 
         int[] dp = new int[nums.length];
-
-        for (int i = 1; i < nums.length; i++) {
-            int min = Integer.MAX_VALUE;
-            for (int j = 0; j < i; j++) {
-                if (nums[j] >= (i - j)) {
-                    min = Math.min(min, dp[j] + 1);
-                }
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 1; i + j < nums.length && j <= nums[i]; j++) {
+                dp[i + j] = Math.min(dp[i + j], dp[i] + 1);
             }
-            dp[i] = min;
         }
         return dp[nums.length - 1];
     }
