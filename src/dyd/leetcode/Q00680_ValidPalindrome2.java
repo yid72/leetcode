94cdb606df543c82ae4212a37d5f1e6d03a725da
yid72 <duyidong@gmail.com>
2020-02-07 02:15:23 -0800

    680. valid palindrome II

--- src/dyd/leetcode/Q00680_ValidPalindrome2.java
+++ src/dyd/leetcode/Q00680_ValidPalindrome2.java
@@ -1,6 +1,8 @@
 package dyd.leetcode;
 
 /**
+ * 680. Valid Palindrome II -- Easy
+ *
  * Given a non-empty string s, you may delete at most one character. Judge whether you can make it a palindrome.
  *
  * Example 1:
@@ -13,33 +15,24 @@ package dyd.leetcode;
  * Note:
  * The string will only contain lowercase characters a-z. The maximum length of the string is 50000.
  */
-public class Q00680 {
+public class Q00680_ValidPalindrome2 {
     public boolean validPalindrome(String s) {
         if (s.length() <= 1) {
             return true;
         }
 
-        if (isPalindrome(s)) {
-            return true;
-        }
-
-        for (int i = 0; i < s.length(); i++) {
-            StringBuilder sb = new StringBuilder(s.substring(0, i));
-            sb.append(s.substring(i + 1));
-            if (isPalindrome(sb.toString())) {
-                return true;
-            }
-        }
-
-        return false;
+        return isPalindrome(s, 0, s.length() - 1, 0);
     }
 
-    private boolean isPalindrome(String s) {
-        int i = 0;
-        int j = s.length() - 1;
+    private boolean isPalindrome(String s, int start, int end, int count) {
+        int i = start;
+        int j = end;
         while (i < j) {
             if (s.charAt(i) != s.charAt(j)) {
-                return false;
+                if (count == 1) {
+                    return false;
+                }
+                return isPalindrome(s, i + 1, j, 1) || isPalindrome(s, i, j - 1, 1);
             }
             i ++;
             j --;
@@ -54,7 +47,7 @@ public class Q00680 {
     }
 
     private static void test(String s) {
-        Q00680 q = new Q00680();
+        Q00680_ValidPalindrome2 q = new Q00680_ValidPalindrome2();
         System.out.println(s + ": " + q.validPalindrome(s));
     }
 }
