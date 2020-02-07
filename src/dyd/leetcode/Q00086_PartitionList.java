e8d5027d0ed8fa6085ce09f727d9a032b3589b87
yid72 <duyidong@gmail.com>
2020-02-06 20:49:31 -0800

    refactor

--- src/dyd/leetcode/Q00086_PartitionList.java
+++ src/dyd/leetcode/Q00086_PartitionList.java
@@ -1,23 +1,23 @@
-package dyd.leetcode.q086;
+package dyd.leetcode;
 
 import dyd.leetcode.common.ListNode;
 import dyd.leetcode.common.ListUtil;
 import dyd.leetcode.common.PrintUtil;
 
 /**
- * 
-Given a linked list and a value x, partition it such that all nodes less than x come before nodes greater than or equal to x.
-
-You should preserve the original relative order of the nodes in each of the two partitions.
-
-For example,
-Given 1->4->3->2->5->2 and x = 3,
-return 1->2->2->4->3->5.
-
- * @author dyd
+ * 86. Partition List -- Medium
+ *
+ * Given a linked list and a value x, partition it such that all nodes less than x come before nodes greater than or equal to x.
+ *
+ * You should preserve the original relative order of the nodes in each of the two partitions.
+ *
+ * Example:
+ *
+ * Input: head = 1->4->3->2->5->2, x = 3
+ * Output: 1->2->2->4->3->5
  *
  */
-public class Q086_PartitionList {
+public class Q00086_PartitionList {
     public ListNode partition(ListNode head, int x) {
     	ListNode lowHead = null, lowCur = null, highHead = null, highCur = null;
     	
@@ -60,7 +60,7 @@ public class Q086_PartitionList {
     }
     
     public static void main(String[] args) {
-    	Q086_PartitionList q = new Q086_PartitionList();
+    	Q00086_PartitionList q = new Q00086_PartitionList();
     	ListNode head = ListUtil.createList(new int[] {1, 4, 3, 2, 5, 2});
     	System.out.print("Original list: ");
     	PrintUtil.printList(head);
